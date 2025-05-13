3d96113aeaff9df92ff788c5464237b53add4eb3
Marcio Pereira <marciohenrique79@gmail.com>
2025-05-13 11:13:17 -0300

    melhoria no formato json lido do csv

--- vinicultura/utilitarios.py
+++ vinicultura/utilitarios.py
@@ -4,8 +4,11 @@ import json
 def recupera_csv(rota=str,ano=str):
     match rota:
         case 'producao':
-            df = pd.read_csv("csv\Producao.csv",delimiter=";")
-            return df[['produto',str(ano)]].to_json()
+            df = pd.read_csv("csv\Producao.csv",delimiter=";")            
+            return {"titulo": "Produção de vinhos e derivados",
+                    "ano": str(ano),
+                    "tipo": [df[['produto',str(ano)]].to_json()]
+                }
         case 'processamento':
             df = pd.read_csv("csv\ProcessaViniferas.csv",delimiter=";")
             j1 = df[['cultivar',str(ano)]].to_json()
@@ -15,10 +18,16 @@ def recupera_csv(rota=str,ano=str):
             j3 = df[['cultivar',str(ano)]].to_json()
             df = pd.read_csv("csv\ProcessaSemclass.csv",delimiter="\t")
             j4 = df[['cultivar',str(ano)]].to_json()
-            return json.dumps({'Viníferas':j1, 'Americanas':j2,'Mesa':j3,'Sem Calsse':j4})
+            return {"titulo": "Processamento",
+                    "ano": str(ano),
+                    "tipo": {"Viníferas":j1, "Americanas":j2,"Mesa":j3,"Sem Classe":j4}
+                }            
         case 'comercializacao':
             df = pd.read_csv("csv\Comercio.csv",delimiter=";")
-            return df[['Produto',str(ano)]].to_json()
+            return {"titulo": "Comercialização de vinhos e derivados",
+                    "ano": str(ano),
+                    "tipo": [df[['Produto',str(ano)]].to_json()]
+                }            
         case 'importacao':
             df = pd.read_csv("csv\ImpVinhos.csv",delimiter="\t")
             j1 = df[['País',str(ano)]].to_json()
@@ -30,7 +39,10 @@ def recupera_csv(rota=str,ano=str):
             j4 = df[['País',str(ano)]].to_json()
             df = pd.read_csv("csv\ImpSuco.csv",delimiter=";")
             j5 = df[['País',str(ano)]].to_json()
-            return json.dumps({'Vinhos':j1, 'Espumantes':j2,'Frescas':j3,'Passas':j4,'Suco':j5})
+            return {"titulo": "Importação",
+                    "ano": str(ano),
+                    "tipo": {"Vinhos":j1, "Espumantes":j2,"Frescas":j3,"Passas":j4,"Suco":j5}
+                }
         case 'exportacao':
             df = pd.read_csv("csv\ExpVinho.csv",delimiter="\t")
             j1 = df[['País',str(ano)]].to_json()
@@ -39,5 +51,8 @@ def recupera_csv(rota=str,ano=str):
             df = pd.read_csv("csv\ExpUva.csv",delimiter="\t")
             j3 = df[['País',str(ano)]].to_json()
             df = pd.read_csv("csv\ExpSuco.csv",delimiter="\t")
-            j4 = df[['País',str(ano)]].to_json()
-            return json.dumps({'Vinhos':j1, 'Espumantes':j2,'Frescas':j3,'Suco':j4})
+            j4 = df[['País',str(ano)]].to_json()            
+            return {"titulo": "Exportação",
+                    "ano": str(ano),
+                    "tipo": {"Vinhos":j1, "Espumantes":j2,"Frescas":j3,"Suco":j4}
+                }
